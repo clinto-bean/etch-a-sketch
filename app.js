0c6859e23e71528ef143110a23de764841a660e7
Clinto Bean <clintonclark1995@hotmail.com>
2023-07-03 01:23:34 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const etchASketch = () => {
+  let pixels
+  let grid = 1
+  const shake = document.getElementById("shake")
+
+  const generateArea = () => {
+    grid = prompt("How many rows and columns do you need?")
+    const canvas = () => {
+      for (let i = 0; i < grid ** 2; i++) {
+        const screen = document.getElementById("screen")
+        screen.style.gridTemplateColumns = "Repeat(" + grid + ", 1fr"
+        pixels = document.querySelectorAll(".pixel")
+        let pixel = document.createElement("div")
+        pixel.classList.add("pixel")
+        pixel.addEventListener("mouseover", () => {
+          addPixel(pixel)
+        })
+        screen.appendChild(pixel)
+      }
+    }
+    canvas(grid)
+  }
+  const addPixel = (pixel) => {
+    if (pixel.classList.contains("clicked")) {
+      pixel.classList.remove("clicked")
+    } else {
+      pixel.classList.add("clicked")
+    }
+  }
+
+  shake.addEventListener("click", () => {
+    pixels.forEach((pixel) => {
+      pixel.classList.remove("clicked")
+    })
+  })
+
+  generateArea()
+}
+etchASketch()
